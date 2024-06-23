67121704920a75ca8e371a54ba91cf7ea4d09695
Kerem Mert <kerem37mert@yahoo.com>
2024-06-23 19:35:12 +0300

    prime number updated

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,9 +4,8 @@ public class PrimeNumber {
 
     public static boolean isPrime(int number) {
         for (int i = 2; i < number/2; i++) {
-            if (number % i == 0) {
+            if (number % i == 0)
                 return false;
-            }
         }
 
         return true;
