c084c15f11ea195a5f437aa52de6358be8215338
Kerem Mert <kerem37mert@yahoo.com>
2024-06-23 19:33:21 +0300

    prime number

---
+++ PrimeNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static boolean isPrime(int number) {
+        for (int i = 2; i < number/2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number...");
+        int number = sc.nextInt();
+
+        if(isPrime(number))
+            System.out.println(number + " is a prime number");
+        else
+            System.out.println(number + " is not a prime number");
+
+    }
+}
