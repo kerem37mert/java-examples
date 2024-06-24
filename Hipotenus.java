06278c1a97eab69533fcec127738a187864c1980
Kerem Mert <kerem37mert@yahoo.com>
2024-06-24 13:58:31 +0300

    Hipotenus

---
+++ Hipotenus.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Hipotenus {
+    public static void main(String[] args) {
+
+        double a, b, c;
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ucgenin birinci dik kenarini giriniz: ");
+        a = sc.nextDouble();
+        System.out.print("Ucgenin ikinci dik kenarini giriniz: ");
+        b = sc.nextDouble();
+
+        // C^2 = A^2 + B^2
+        c = Math.sqrt((a*a) + (b*b));
+
+        System.out.println("Hipotenus: " + c);
+    }
+}
